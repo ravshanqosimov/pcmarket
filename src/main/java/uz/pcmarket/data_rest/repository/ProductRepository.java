2ae1cf9204b67fc40b3f1a651a27b431b555a363
RavshanQosimov <74915310+ravshanqosimov@users.noreply.github.com>
2021-04-09 14:46:27 +0500

    'fc_created'

--- src/main/java/uz/pcmarket/data_rest/repository/ProductRepository.java
+++ src/main/java/uz/pcmarket/data_rest/repository/ProductRepository.java
@@ -1,10 +1,35 @@
 package uz.pcmarket.data_rest.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import uz.pcmarket.data_rest.entity.Address;
+import uz.pcmarket.data_rest.entity.Category;
 import uz.pcmarket.data_rest.entity.Product;
 
-@RepositoryRestResource(path = "product" )
-public interface ProductRepository extends JpaRepository<Product,Integer> {
+@RepositoryRestResource(path = "byProduct")
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+
+    @RestResource(path = "byName")
+    Page<Product> findAllByName(@Param("name") String name, Pageable pageable);
+
+    @RestResource(path = "byModel")
+    Page<Product> findAllByModel(String model, Pageable pageable);
+
+
+    @RestResource(path = "byDescription")
+    Page<Product> findAllByDescription(String description, Pageable pageable);
+
+    @RestResource(path = "byPrice")
+    Page<Product> findAllByPrice(Double price, Pageable pageable);
+
+    @RestResource(path = "byCategory")
+    Page<Product> findAllByCategory(Category category, Pageable pageable);
+
+    @RestResource(path = "byActive")
+    Page<Product> findAllByActive(boolean active, Pageable pageable);
 }
